76af928fe0371dcb03012153433bd8de186003b0
a66598a <84500884+a66598a@users.noreply.github.com>
2023-03-30 07:40:04 +0800

    Add files via upload

---
+++ HW02解法2-2.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 29 16:48:17 2023
+
+@author: user
+"""
+
+num = list()
+
+sort1 = []
+
+sort2 = []
+
+for i in range(0,6):
+    n = int(input('請輸入數字:'))
+    num.append(n)
+    sort1 = num
+     
+while len(sort1) > 0:
+    m = min(sort1)
+    sort1.remove(m)
+    sort2.append(m)
+    if sort2.count(m) > 1:
+        index = sort2.index(m)
+        sort2.remove(m)
+        y = int(input('請輸入不相同之數字:'))
+        sort2.append(y)
+        
+        
+print(sort2)
