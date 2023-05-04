108b71848ec5f550656a62fbf6ca6e54b4471f4a
a66598a <84500884+a66598a@users.noreply.github.com>
2023-05-04 18:35:41 +0800

    Add files via upload

--- HW01.py
+++ HW01.py
@@ -1,18 +1,41 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Mar 28 20:24:18 2023
+Created on Wed May  3 19:43:26 2023
 
-@author: USER
+@author: user
 """
+import random
 
-scores = [100,60,70,80,99,100,66]
+sum1 = 0
+list1 = []
+list2 = []
 
-n = int(input('請輸入分數:'))
+for i in range(4):
+    num = int(random.randint(1, 6))
+    list1.append(num)
+
+print(list1)
+
+for i in range(1,7):
     
-    if scores.count(n) > 0:
-        index = scores.index(n)
-        scores.pop(index)
-        print(scores)
+    if list1.count(i) == 1:
+        pass
     
-    elif scores.count(n) == 0:
-        print('找不到')
+    elif list1.count(i) >= 2 and list1.count(i) <= 3:
+        list2.append(i)
+        #print(list2)
+
+        if len(list2) == 2:
+            num2 = max(list2)
+            sum2 = 2 * num2
+            print('點數:',sum2)
+        
+    elif list1.count(i) == 4:
+        print('一色最大')
+        
+        
+if len(list2) == 1:
+    num1= max(list2)
+    sum1 += 2 * num1
+    print('點數:',sum1)
+
